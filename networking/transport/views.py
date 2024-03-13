679fa1c07928998d583e80966c94d1d81477da87
stelluchchka <stelluchchka@gmail.com>
2024-03-13 15:53:01 +0300

    add show_pic

--- networking/transport/views.py
+++ networking/transport/views.py
@@ -10,8 +10,18 @@ import requests
 # from kafka import KafkaProducer
 # from kafka import KafkaConsumer
 import json
+from PIL import Image
+import io
 
 Messages = []
+
+class class_message:
+    def __init__(self, time, segment_data, segment_len, segment_num):
+        self.segment_data = segment_data
+        self.time = time
+        self.segment_len = segment_len
+        self.segment_num = segment_num
+
 
 class SendSegmentView(APIView):
     def get(self, request):
@@ -26,7 +36,7 @@ class SendSegmentView(APIView):
             return Response(serializer.errors)
         cur_message = serializer.save()
         print(cur_message.segments_len)
-        channel_service_url = "http://192.168.1.225:8000/dl/"
+        channel_service_url = "http://localhost:8000/transfer/"
         for i in range(cur_message.segments_len):
             cur_message.add_segment(i, base64.b64decode((cur_message.binary_file[i * 1000:(i+1) * 1000])))
             data={
@@ -37,7 +47,7 @@ class SendSegmentView(APIView):
             }
             # print(len(''.join([f'{j:08b}' for j in base64.b64decode(cur_message.binary_file[i * 1000:(i+1) * 1000])])))
             print(len(cur_message.binary_file[i * 1000:(i+1) * 1000]))
-            response = requests.get(channel_service_url,data=data)
+            response = requests.post(channel_service_url,data=data)
             # print(response.status_code)
         message = Message.objects.latest('id')
         segments = Segment.objects.filter(message=message).order_by('segment_num')
@@ -57,12 +67,18 @@ class TransferSegmentView(APIView):
 
         # producer = KafkaProducer(bootstrap_servers='localhost:9000')
 
-        data = {
-            'id': request.data['time'],
-            'segment_data': request.data['segment_data'],
-            'segment_len': int(request.data['segment_len']),
-            'segment_num': int(request.data['segment_num'])
-        }
+        id = request.data['time']
+        segment_data=request.data['segment_data']
+        segment_len = int(request.data['segment_len'])
+        segment_num = int(request.data['segment_num'])
+        
+        # data = {
+        #     'id': request.data['time'],
+        #     'segment_data': request.data['segment_data'],
+        #     'segment_len': int(request.data['segment_len']),
+        #     'segment_num': int(request.data['segment_num'])
+        # }
+
         # value_to_send = json.dumps(data).encode('utf-8')
 
         # future = producer.send('product_topic', key=json.dumps(id).encode('utf-8'), value=value_to_send)
@@ -70,15 +86,21 @@ class TransferSegmentView(APIView):
         # producer.flush()
         # # producer.send('product_topic', key=b'product_category_id', value=b'product_data')
 
-        Messages.append(data)
+
+        ms = class_message(id, segment_data, segment_len, segment_num)
+        Messages.append(ms)
 
         if int(request.data['segment_num']) == int(request.data['segment_len']) - 1:
-            file = ''
+            str_file = ''
             # id = data.id
-            Messages.sort(key=lambda x: x['segment_num'])
+            Messages.sort(key=lambda x: x.segment_num)
             for i in range(int(request.data['segment_len'])):
-                print(i)
-            print(file)
+                str_file += Messages[i].segment_data
+            # print(file)
+            image_bytes = base64.b64decode(str_file)
+            image_data = io.BytesIO(image_bytes)
+            image = Image.open(image_data)
+            image.show()
 
         return Response({"message": "success"})
 
